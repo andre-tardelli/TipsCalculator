8f460c61fe6494f6ece6c649862140940113cddc
Andre Tardelli <andre.ltardelli@gmail.com>
2024-07-19 17:59:28 +0100

    Início criação da tela Summary

--- app/src/main/java/com/example/tipscalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipscalculator/MainActivity.kt
@@ -97,15 +97,19 @@ class MainActivity : AppCompatActivity() {
                     putExtra("totalAmount", totalWithTips)
 
                 }
+                clean()
                 startActivity(intent)
             }
         }
 
         binding.btnClean.setOnClickListener {
-            binding.tieTotal.setText("")
-            binding.rbOptionOne.isChecked = false
-            binding.rbOptionTwo.isChecked = false
-            binding.rbOptionThree.isChecked =false
+            clean()
         }
+    }
+    private fun clean() {
+        binding.tieTotal.setText("")
+        binding.rbOptionOne.isChecked = false
+        binding.rbOptionTwo.isChecked = false
+        binding.rbOptionThree.isChecked =false
     }
 }
--- app/src/main/java/com/example/tipscalculator/SummaryActivity.kt
+++ app/src/main/java/com/example/tipscalculator/SummaryActivity.kt
@@ -5,16 +5,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tipscalculator.databinding.ActivitySummaryBinding
 
 class SummaryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySummaryBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_summary)
+        binding = ActivitySummaryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val totalTable = intent.getFloatExtra("totalTable", 0.0f)
         val numPeople = intent.getFloatExtra("numPeople", 0.0f)
         val percentage = intent.getFloatExtra("percentage", 0.0f)
         val totalAmount = intent.getFloatExtra("totalAmount", 0.0f)
-        println("teste, $totalAmount")
+
+        binding.tvPercentage.text = percentage.toString()
+        binding.tvTotalTable.text = totalTable.toString()
+        binding.tvNumOfPeople.text = numPeople.toString()
+        binding.tvTotalAmount.text = totalAmount.toString()
+
+        binding.btnRefresh.setOnClickListener {
+            finish()
+        }
     }
 }
